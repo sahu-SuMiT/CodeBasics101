9f0194660450dca24377fb0b66b4e46b10a1be68
SuMiT <145762361+sahu-TiM@users.noreply.github.com>
2023-10-13 16:40:23 +0530

    Update quadratic_equ.c

--- quadratic_equ.c
+++ quadratic_equ.c
@@ -1,21 +1,32 @@
 // Write a C program to input the coefficients of a quadratic equation and find the roots of equation Ryan BCS057
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
 int main() {
-    float a,b,c;
+    float a,b,c,alpha,beta,discriminant;
     printf("ax^2+bx+c=0 is quadratic equation\n");
-    printf("Enter coefficient of x^2: ");
-    scanf("%f", &a);
-    printf("Enter coefficient of x: ");
-    scanf("%f", &b);
-    printf("Enter constant: ");
-    scanf("%f", &c);
-    float root1,root2;
-    float srinum=sqrt((b*b)-(4*a*c));
-    root1=((-b)+(srinum))/(2*a);
-    root2=((-b)-(srinum))/(2*a);
-    printf("Root 1 is %f\n",root1);
-    printf("Root 2 is %f\n",root2);
+    printf("Enter coefficient a b c with gap between them : ");
+    scanf("%f %f %f", &a,&b,&c);
+    discriminant=b*b-4*a*c;
+    
+    if(discriminant>0){
+        printf("Real & Distinct roots \n");
+        alpha=(-b+sqrt(discriminant))/2;
+        beta=(-b-sqrt(discriminant))/2;
+        printf("The roots are %f & %f\n",alpha,beta);   
+    }
+
+    if(discriminant==0){
+        printf("Real & Equal roots \n");
+        alpha=-b/2;
+        printf("The root is %f \n",alpha);  
+    }
+
+    if(discriminant<0){                                        
+        printf("Imaginary roots \n");
+        discriminant=-discriminant;
+        alpha=-b/2;                      // alpha=real part,
+        beta=sqrt(discriminant)/2;       // beta=imaginary part
+        printf("The roots are %f + %fi  &  %f - %fi \n",alpha,beta,alpha,beta);
+    }
     return 0;
 }   
