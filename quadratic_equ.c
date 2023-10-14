553acda5018d2a502c2a4338d74107ef0dd3ca3d
SuMiT <145762361+sahu-TiM@users.noreply.github.com>
2023-10-14 22:30:33 +0530

    Update quadratic_equ.c
    made the discriminant more readable :)

--- quadratic_equ.c
+++ quadratic_equ.c
@@ -5,16 +5,14 @@
 int main() {
     float a,b,c;
     printf("ax^2+bx+c=0 is quadratic equation\n");
-    printf("Enter coefficient of x^2: ");
+    printf("Enter coefficient a b c with gap: ");
     scanf("%f", &a);
-    printf("Enter coefficient of x: ");
     scanf("%f", &b);
-    printf("Enter constant: ");
     scanf("%f", &c);
     float root1,root2;
-    float srinum=sqrt((b*b)-(4*a*c));
-    root1=((-b)+(srinum))/(2*a);
-    root2=((-b)-(srinum))/(2*a);
+    float discriminant=((b*b)-(4*a*c));
+    root1=((-b)+sqrt(discriminant)/(2*a);
+    root2=((-b)-sqrt(discriminant))/(2*a);
     printf("Root 1 is %f\n",root1);
     printf("Root 2 is %f\n",root2);
     return 0;
